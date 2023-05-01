b508cf20646a8d7a4cf857dc2ba489bc7d0d2af6
Samuel Rodrigo Baioco <rdrgbaioco@gmail.com>
2023-05-01 14:32:39 -0400

    add example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,65 +1,75 @@
-import 'package:dynamic_badges/dynamic_badges.dart';
 import 'package:flutter/material.dart';
+import 'package:dynamic_tools/dynamic_tools.dart';
 
 void main() {
-  runApp(const BadgeApp());
+  runApp(const MyApp());
 }
 
-class BadgeApp extends StatelessWidget {
-  const BadgeApp({Key? key}) : super(key: key);
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: BadgeExample(),
+    return MaterialApp(
+      title: 'Flutter Demo',
+      themeMode: context.isDarkMode ? ThemeMode.dark : ThemeMode.light,
+      theme: ThemeData(
+        brightness: Brightness.light,
+        colorSchemeSeed: Colors.blue,
+      ),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+        colorSchemeSeed: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class BadgeExample extends StatelessWidget {
-  const BadgeExample({Key? key}) : super(key: key);
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            DynamicBadge(
-              label: "3",
-              childSize: 92,
-              child: IconButton(
-                iconSize: 92,
-                icon: const Icon(
-                  Icons.chat_bubble_outline_rounded,
-                ),
-                onPressed: () {},
-              ),
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
             ),
-            DynamicBadge.small(
-              childSize: 92,
-              child: IconButton(
-                iconSize: 92,
-                icon: const Icon(
-                  Icons.chat_bubble_outline_rounded,
-                ),
-                onPressed: () {},
-              ),
-            ),
-            DynamicBadge.counter(
-              count: 1323,
-              childSize: 92,
-              child: IconButton(
-                iconSize: 92,
-                icon: const Icon(
-                  Icons.chat_bubble_outline_rounded,
-                ),
-                onPressed: () {},
-              ),
+            Text(
+              '$_counter',
+              style: context.textTheme.headlineMedium, // To get theme
             ),
           ],
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
       ),
     );
   }
--- example/test/widget_test.dart
+++ example/test/widget_test.dart
@@ -1,19 +1,30 @@
-import 'package:example/main.dart';
+// This is a basic Flutter widget test.
+//
+// To perform an interaction with a widget in your test, use the WidgetTester
+// utility in the flutter_test package. For example, you can send tap and scroll
+// gestures. You can also use WidgetTester to find child widgets in the widget
+// tree, read text, and verify that the values of widget properties are correct.
+
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:dynamic_badges/dynamic_badges.dart';
+
+import 'package:example/main.dart';
 
 void main() {
-  testWidgets('Badge Example', (WidgetTester tester) async {
-    await tester.pumpWidget(const BadgeApp());
+  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(const MyApp());
 
-    final columnFinder = find.byType(Column);
-    expect(columnFinder, findsOneWidget);
+    // Verify that our counter starts at 0.
+    expect(find.text('0'), findsOneWidget);
+    expect(find.text('1'), findsNothing);
 
-    final textFinder = find.text('Badge Example');
-    expect(textFinder, findsOneWidget);
+    // Tap the '+' icon and trigger a frame.
+    await tester.tap(find.byIcon(Icons.add));
+    await tester.pump();
 
-    final dynamicBadgeTestFinder = find.byType(DynamicBadge);
-    expect(dynamicBadgeTestFinder, findsNWidgets(2));
+    // Verify that our counter has incremented.
+    expect(find.text('0'), findsNothing);
+    expect(find.text('1'), findsOneWidget);
   });
 }
